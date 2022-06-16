83753084ad58f1273b114f37a0bea692b1bc61d4
ymink716 <ymink716@gmail.com>
2022-06-16 18:05:54 +0900

    Feat: 채용공고 등록, 수정에 에러처리 추가
    레파지토리 createPost, updatePost 메서드에 company나 post가 존재하지 않을 시 에러처리 추가

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -1,7 +1,8 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, UpdateResult } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { Posts } from '../entities/posts.entity';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @EntityRepository(Posts)
 export class PostsRepository extends Repository<Posts> {
@@ -15,6 +16,10 @@ export class PostsRepository extends Repository<Posts> {
       .where('company.id = :id', { id: company_id })
       .getOne();
 
+    if (!company) {
+      throw new NotFoundException('존재하지 않는 회사입니다.');
+    }
+
     const post: Posts = this.create({
       company,
       position,
@@ -23,9 +28,6 @@ export class PostsRepository extends Repository<Posts> {
       description,
     });
 
-    // const result = this.createQueryBuilder()
-    //   .insert('')
-
     await this.save(post);
     return post;
   }
@@ -33,14 +35,18 @@ export class PostsRepository extends Repository<Posts> {
   async updatePost(id: number, updatePostDto: UpdatePostDto): Promise<Posts> {
     const { position, compensation, tech, description } = updatePostDto;
 
-    const post = await this.createQueryBuilder()
+    const result: UpdateResult = await this.createQueryBuilder()
       .update(Posts)
       .set({ position, compensation, tech, description })
       .where('id = :id', { id })
       .returning('*')
       .execute();
 
-    return post.raw[0];
+    if (result.affected === 0) {
+      throw new NotFoundException('해당 공고를 찾을 수 없습니다.');
+    }
+
+    return result.raw[0];
   }
 
   async getPostByKeyword(keyword: string): Promise<Posts[]> {
